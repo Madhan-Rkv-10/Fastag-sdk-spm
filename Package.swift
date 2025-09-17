14f7f596374ff1498e7cbb10326db55e1f500c05
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-09-17 19:32:18 +0530

    remove debug symbols

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FastagSdkIos",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "FastagSdkIos",
+            targets: ["FastagSdkIos"]),
+    ],
+    targets: [
+        .target(
+            name: "FastagSdkIos",
+            dependencies: [
+                "App",
+                "fastagwrapperios",
+                "Flutter"
+            ],
+            path: "Sources/FastagSdkIos"
+        ),
+        
+        .binaryTarget(
+            name: "fastagwrapperios",
+            path: "./fastagwrapperios.xcframework"
+        ),
+        
+        .binaryTarget(
+            name: "App",
+            path: "./App.xcframework"
+        ),
+        .binaryTarget(
+            name: "Flutter",
+            path: "./Flutter.xcframework"
+        )
+        
+    ]
+)
+
+
